7de8bcf409b2fdfeb42375d499c1860f0a34a2ab
xcxcxz <xcxcxz@naver.com>
2021-06-24 10:31:59 +0900

    ..

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    /** mainActivity 최초실행시 실행
+    */
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //UI 세팅
         setContentView(R.layout.activity_main);
+        //Toast 메세지
+        Toast.makeText(getApplicationContext(), "프로그래밍 시작", Toast.LENGTH_LONG).show();
     }
 }
