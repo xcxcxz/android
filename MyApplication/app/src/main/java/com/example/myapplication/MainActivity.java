79ba84b2f8c2365e97d56612ba9b025ccf651e7f
xcxcxz <xcxcxz@naver.com>
2021-06-24 17:23:44 +0900

    ..

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    
+    int clickCount = 0;
 
     /** mainActivity 최초실행시 실행
     */
@@ -16,5 +19,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //Toast 메세지
         Toast.makeText(getApplicationContext(), "프로그래밍 시작", Toast.LENGTH_LONG).show();
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickCount = clickCount +  1;
+
+                String text = "";
+
+                for(int i=0; i<clickCount;i++){
+                    if(i>= 3){
+                        break;
+                    }
+                    text = text + "HIHI";
+                }
+                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
